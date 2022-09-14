0da8b9b718ce12479445358e917c0dfc8501c7c7
Левашов А.В <u0250@C0250.fc.chromatec.ru>
2022-09-14 12:42:57 +0300

    *

---
+++ app_server/controllers/main.js
@@ -0,0 +1,3 @@
+module.exports.index = function(reg, res) {
+    res.render('index', { title: 'Express'});
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+var homepageController = function(reg, res) {
+  res.render('index', {title: 'Express'})
+};
+
+router.get('/', ctrlMain.index);
+
+/* Get home page. */
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
 
 module.exports = router;
